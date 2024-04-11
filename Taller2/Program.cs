931e48d01c12968ce7c1eab8453b77150616bcc6
Geeky346 <110034374+Geeky346@users.noreply.github.com>
2024-04-11 10:50:00 -0500

    Update Program.cs

--- Taller2/Program.cs
+++ Taller2/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace Taller2
 {
-    internal class Program
+public delegate int OperacionX(int n, int a);
+public class Program
+{
+    //Suma
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
-    }
+       OperacionX Operacion = Suma;
+       OperacionX Operacion1 = Resta;
+       OperacionX Operacion2 = Multi;
+       OperacionX Operacion3 = Divi;
+       OperacionX Operacion4 = Residuo;
 }
