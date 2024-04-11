cf414bd68bc24782c2d04ff2ee7c8ccd6616a166
quertuy <36798819+quertuy@users.noreply.github.com>
2024-04-11 10:48:06 -0500

    Funcion suma Delegate

---
+++ Delegado Funcion/DelegadoJs.cs
@@ -0,0 +1,35 @@
+﻿namespace delegado
+{
+    internal class Program
+    {
+        // Funciones para realizar las operaciones matemáticas
+        static double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Dividir(double a, double b)
+        {
+            if (b != 0)
+                return a / b;
+            else
+                throw new ArgumentException("No se puede dividir por cero.");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
